d55dd27102f7cb3febeb1f477337bd960679b3e4
ArijitRoy-Coditas <arijit.roy@coditas.com>
2025-04-29 15:59:21 +0530

    Changed model to show output

--- src/admin-app/authentication/models/createUserModel.go
+++ src/admin-app/authentication/models/createUserModel.go
@@ -1,20 +1,19 @@
 package models
 
 type BFFCreateUserRequest struct {
-	Name        int `json:"name"`
-	Username    int `json:"username"`
-	PhoneNumber int `json:"phoneNumber"`
-	Password    int `json:"password"`
-	PanCard     int `json:"panCard"`
-	Email       int `json:"email"`
+	Name        int         `json:"name"`        // int → will convert to string
+	Username    string      `json:"username"`    // string → int
+	PhoneNumber interface{} `json:"phoneNumber"` // float64 → uint64
+	Password    string      `json:"password"`    // same
+	PanCard     float64     `json:"panCard"`     // float64 → int64
+	Email       int         `json:"email"`       // int → string
 }
 
 type BFFCreateUserResponse struct {
-	Name        string `json:"name"`
-	Username    string `json:"username"`
-	PhoneNumber string `json:"phoneNumber"`
-	Password    string `json:"password"`
-	PanCard     string `json:"panCard"`
-	Email       string `json:"email"`
-	Message     string `json:"message"`
+	Name        string `json:"name"`        // from int → string
+	Username    int    `json:"username"`    // from string → int
+	PhoneNumber uint64 `json:"phoneNumber"` // from interface(float64) → uint64
+	Password    string `json:"password"`    // same
+	PanCard     int64  `json:"panCard"`     // from float64 → int64
+	Email       string `json:"email"`       // from int → string
 }
